9f9225a47002707d8676cd2ad3eb2769cd80a85e
Harry Gee <code@pibot.org>
2017-10-09 13:12:32 +0100

    Update test2.sh

--- config/test2.sh
+++ config/test2.sh
@@ -89,6 +89,31 @@ set +x
 ##-------------------------------------------------------------------------------------------------
 ##-------------------------------------------------------------------------------------------------
 
+## Enable Serial Port
+# Findme look at using sed to toggle it
+echo 'Enabling Serial Port...'
+if grep -q 'enable_uart=1' /boot/config.txt; then
+    echo 'enable_uart=1 is already set - skipping'
+else
+    echo 'ena
+    ble_uart=1' | sudo tee -a /boot/config.txt
+fi
+
+## Disable Serial login
+echo 'Disabling Serial Login...'
+    # Rpi 3
+    systemctl stop serial-getty@ttyS0.service
+    systemctl disable serial-getty@ttyS0.service
+
+## Disable Boot info
+echo 'Disabling Boot info...'
+#sudo sed -i'bk' -e's/console=ttyAMA0,115200.//' -e's/kgdboc=tty.*00.//'  /boot/cmdline.txt
+sed -i'bk' -e's/console=serial0,115200.//'  /boot/cmdline.txt
+
+
+##-------------------------------------------------------------------------------------------------
+
+
 ## Install Arduino
 echo 'Installing Arduino IDE...'
 program="arduino"
@@ -101,7 +126,6 @@ if [ $condition -eq 0 ] ; then
 else
     echo "Arduino IDE is already installed - skipping"
 fi
-
 
 
 ## Setup the Reset Pin
