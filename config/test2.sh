94d95dc70ba81ad1497f69e1c7656cabf132aace
Harry Gee <code@pibot.org>
2017-10-09 10:31:48 +0100

    Update test2.sh

--- config/test2.sh
+++ config/test2.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-
-# trap "set +x; sleep 5; set -x" DEBUG
+echo -e "\E[32m"
+echo " ____  __  ____   __  ____      ____  ____  ____  _  _  ____  ";
+echo "(  _ \(  )(  _ \ /  \(_  _)    / ___)(  __)(_  _)/ )( \(  _ \ ";
+echo " ) __/ )(  ) _ ((  O ) )(      \___ \ ) _)   )(  ) \/ ( ) __/ ";
+echo "(__)  (__)(____/ \__/ (__)     (____/(____) (__) \____/(__)   ";
+echo -e "\E[0m"
+echo "Welcome to the PiBot Setup Installer."
+echo " "
+echo "This installation script has the following requirements:"
+echo "1) Script must be run as root user"
+echo "2) There must be a working internet connection"
+echo "3) Currently this script is only compatible with the Raspian Strech on a Raspberry Pi v3"
+echo " "
+echo "Will now perfrom tests for these requirements before continuing ...."
+echo " "
+echo "1: Checking for execution as root...."
 
 # Check whether script is being run as sudo
 if [[ $EUID -ne 0 ]]; then
@@ -8,6 +22,19 @@ if [[ $EUID -ne 0 ]]; then
   	exit 1
 fi
 
+echo " "
+echo "Root execution: PASSED. Now checking for internet connectivity..."
+echo "=================================="
+wget -q --tries=2 --timeout=100 http://google.com -O /dev/null
+if [ $? -eq 0 ];then
+        echo "Internet Connection Verified"
+else
+        echo "Unable to Connect, try again !!!"
+        exit 0
+fi
+
+echo " "
+echo "Now checking for compatible OS version..."
 # check if system is using the latest 'stretch' version of Raspian
 osInfo=$(cat /etc/os-release)
 if [[ $osInfo == *"stretch"* ]]; then
