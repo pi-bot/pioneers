f97536849c64daa1454f587e96022a1603591a6c
Harry Gee <code@pibot.org>
2017-11-15 10:11:34 +0000

    Create configureSerial.sh

---
+++ config/configureSerial.sh
@@ -0,0 +1,26 @@
+!/bin/bash
+# This script can be run from the command line using 'sudo bash configureSerial.sh' (assuming your in the same directory)
+# This script reconfigures the RaspiPi serial interface for use with the PiBot development board.
+##-------------------------------------------------------------------------------------------------
+
+## Enable Serial Port
+# Findme look at using sed to toggle it
+echo 'Enabling Serial Port...'
+if grep -q 'enable_uart=1' /boot/config.txt; then
+    echo 'enable_uart=1 is already set - skipping'
+else
+    echo 'enable_uart=1' | sudo tee -a /boot/config.txt
+fi
+
+## Disable Serial login
+echo 'Disabling Serial Login...'
+    # Rpi 3
+    systemctl stop serial-getty@ttyS0.service
+    systemctl disable serial-getty@ttyS0.service
+
+## Disable Boot info
+echo 'Disabling Boot info...'
+#sudo sed -i'bk' -e's/console=ttyAMA0,115200.//' -e's/kgdboc=tty.*00.//'  /boot/cmdline.txt
+sed -i'bk' -e's/console=serial0,115200.//'  /boot/cmdline.txt
+
+##-------------------------------------------------------------------------------------------------
