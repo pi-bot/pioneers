34a23594ad2dbc7d9f4626ee86dac51f623c3e39
Harry Gee <code@pibot.org>
2017-11-15 14:49:14 +0000

    Update installPiBotFiles.sh

--- config/installPiBotFiles.sh
+++ config/installPiBotFiles.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 # This script can be run from the command line using 'sudo bash installPiBotFiles.sh' (assuming your in the same directory)
 # This script reconfigures the RaspiPi serial interface for use with the PiBot development board.
 ##-------------------------------------------------------------------------------------------------
@@ -15,25 +15,25 @@ else
 	mv /usr/share/arduino/examples /usr/share/arduino/examples.backup
 fi
 
-cp ~/pioneers/examples /usr/share/arduino/examples
+cp ../examples /usr/share/arduino/examples
 
 ##-------------------------------------------------------------------------------------------------
 
 echo "Now replacing the Arduino IDE's library directories"
 
-cd /usr/share/arduino/libraries-backUp
-
 if [ -f "/usr/share/arduino/libraries.backup" ]
 then
 	echo "library directory already backed up."
-  rm -rf /usr/share/arduino/libraries
+	rm -rf /usr/share/arduino/libraries
 else
-  mkdir /usr/share/arduino/libraries.backup
-  mv /usr/share/arduino/libraries/* /usr/share/arduino/libraries.backup/
+ 	mkdir /usr/share/arduino/libraries.backup
+	mv /usr/share/arduino/libraries/* /usr/share/arduino/libraries.backup/
 fi
 
 cd /usr/share/arduino/libraries
 git clone https://github.com/pi-bot/NeoPixel
 git clone https://github.com/pi-bot/DC_motor-driver
+
+echo "Arduino library configured and installation script complete. "
 
 ##-------------------------------------------------------------------------------------------------
