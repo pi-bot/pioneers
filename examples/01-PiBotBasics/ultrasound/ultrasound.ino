bc91aa1224bd65b91adff25b887b7b439061ed1f
Harry Gee <code@pibot.org>
2017-11-19 23:04:13 +0000

    Update ultrasound.ino

--- examples/01-PiBotBasics/ultrasound/ultrasound.ino
+++ examples/01-PiBotBasics/ultrasound/ultrasound.ino
@@ -1,30 +1,21 @@
-#include "Arduino.h"
-
-int trigPin = 14;    //Trig - green Jumper
-int echoPin = 14;    //Echo - yellow Jumper
-long duration, cm, inches;
-
+/*
+Uses the new ping library from: http://playground.arduino.cc/Code/NewPing
+*/
+#include <NewPing.h>
+ 
+#define TRIGGER_PIN  14
+#define ECHO_PIN     17
+#define MAX_DISTANCE 200
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+ 
 void setup() {
-   Serial.begin(9600);
-   int readUltrasound(int triggerPin)
-{
-  pinMode(triggerPin, OUTPUT);
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(4);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(triggerPin, INPUT);
-  long duration = pulseIn(triggerPin, HIGH, 100000);//0.1 second of timeout
-  long distance = (duration/2) / 29.1; //distance in cm
-  return distance;
-};
-    
+  Serial.begin(115200);
 }
-
-void loop()
-{
-   unsigned int uS =readUltrasound(trigPin);
-   Serial.print(uS);
-     delay(250);
-  }
+ 
+void loop() {
+  delay(50);
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm());
+  Serial.println("cm");
+}
