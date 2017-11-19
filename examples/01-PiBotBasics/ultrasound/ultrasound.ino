1928fe995b44d0a0a66b3753b9cbce37a275c53e
Harry Gee <code@pibot.org>
2017-11-19 23:57:30 +0000

    Update ultrasound.ino

--- examples/01-PiBotBasics/ultrasound/ultrasound.ino
+++ examples/01-PiBotBasics/ultrasound/ultrasound.ino
@@ -1,21 +1,80 @@
-/*
-Uses the new ping library from: http://playground.arduino.cc/Code/NewPing
-*/
-#include <NewPing.h>
- 
-#define TRIGGER_PIN  14
-#define ECHO_PIN     17
-#define MAX_DISTANCE 200
- 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
- 
+/**
+ * HC-SR04 Demo
+ * Demonstration of the HC-SR04 Ultrasonic Sensor without using a library
+ * 
+ * Description:
+ *  Run the sketch and open a serial
+ *  monitor. The distance read from the sensor will be displayed
+ *  in centimeters and inches.
+ * 
+ * Hardware Connections:
+ *  PiBot | HC-SR04 
+ *  -------------------
+ *    5V    |   VCC     
+ *    14    |   Trig     
+ *    17    |   Echo     
+ *    GND   |   GND
+ *  
+ * License:
+ *  Public Domain
+ */
+
+// Pins
+const int TRIG_PIN = 14;
+const int ECHO_PIN = 17;
+
+// Anything over 400 cm (23200 us pulse) is "out of range"
+const unsigned int MAX_DIST = 23200;
+
 void setup() {
-  Serial.begin(115200);
+
+  // The Trigger pin will tell the sensor to range find
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // We'll use the serial monitor to view the sensor output
+  Serial.begin(9600);
 }
- 
+
 void loop() {
-  delay(50);
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm());
-  Serial.println("cm");
+
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+  float inches;
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO_PIN) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO_PIN) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance in centimeters and inches. The constants
+  // are found in the datasheet, and calculated from the assumed speed 
+  //of sound in air at sea level (~340 m/s).
+  cm = pulse_width / 58.0;
+  inches = pulse_width / 148.0;
+
+  // Print out results
+  if ( pulse_width > MAX_DIST ) {
+    Serial.println("Out of range");
+  } else {
+    Serial.print(cm);
+    Serial.print(" cm \t");
+    Serial.print(inches);
+    Serial.println(" in");
+  }
+  
+  // Wait at least 60ms before next measurement
+  delay(60);
 }
