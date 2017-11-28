1acff65718e08468a8ca8aa9cbf50dfa6e46a72c
Harry Gee <code@pibot.org>
2017-11-28 14:03:33 +0000

    Create roboNavigator2.py

---
+++ python/roboNavigator2.py
@@ -0,0 +1,53 @@
+import serial
+import time
+import pygame
+
+sendCmd = serial.Serial('/dev/ttyS0', 9600, timeout = 1)
+time.sleep(2)
+
+screen = pygame.display.set_mode([300,100])
+screen.fill([255,255,255])
+pygame.display.set_caption("Windows Controller")
+
+
+def forward():
+   sendCmd.write("w")
+   print("Forward")
+def backward():
+   sendCmd.write("x")
+   print("Backward")
+def left():
+   sendCmd.write("a")
+   print("Left")
+def right():
+   sendCmd.write("d")
+   print("Right")
+def stop():
+   sendCmd.write("x")
+   print("Stop")
+
+while 1:
+   key = pygame.key.get_pressed()
+   for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           break
+       elif event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_UP:
+                   print"Forward"
+                   forward()
+           if event.key == pygame.K_LEFT:
+                   print"Left"
+                   left()
+           if event.key == pygame.KEY_DOWN:
+                   print"Backward"
+                   backward()
+           if event.key == pygame.K_RIGHT:
+                   print"Right"
+                   right()
+           if event.key == pygame.K_ESCAPE:
+                   break
+       elif event.type == pygame.KEYUP:
+                   print"Stop"
+                   stop()
+   pygame.display.update()
+pygame.quit
