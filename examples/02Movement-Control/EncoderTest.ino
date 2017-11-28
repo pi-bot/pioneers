f925e6fbcb4950a0e6e73bfb1f9393ae91cbbb37
Harry Gee <code@pibot.org>
2017-11-28 21:16:54 +0000

    Create EncoderTest.ino

---
+++ examples/02Movement-Control/EncoderTest.ino
@@ -0,0 +1,8 @@
+// Please ignore this file it is work in progress:
+
+
+
+//                           _______         _______       
+//               Pin1 ______|       |_______|       |______ Pin1
+// negative <---         _______         _______         __      --> positive
+//               Pin2 __|       |_______|       |_______|   Pin2
